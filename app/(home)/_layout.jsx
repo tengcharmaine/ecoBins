b540465a639d3986df1a30860c267d3ddaf4c908
tengcharmaine <e0969596@u.nus.edu>
2023-06-01 16:33:19 +0800

    Squashed commit of the following:
    commit caa8cba447ac8338589d276cc86eaf0907c6cd57
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Thu Jun 1 16:30:07 2023 +0800
    
        update vr2
    
    commit 0d967e2cddd144638ef0c144817029a991c4bfc6
    Merge: 4bbfcda 2b41c53
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Thu Jun 1 16:17:18 2023 +0800
    
        Merge branch 'main' into milestone1
    
    commit 4bbfcdac4d32b179b3dadbc444c00876aef3bc37
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Thu Jun 1 16:11:30 2023 +0800
    
        profile page update
    
    commit 403f563e1e6ed69111cbf83c86fe739ec0e348f7
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Sat May 27 21:32:39 2023 +0800
    
        Draft 3.1
    
    commit 1f93f579c8b87ec455573f3a1f2e00565bbc7276
    Author: jiahui0309 <121473213+jiahui0309@users.noreply.github.com>
    Date:   Sat May 27 21:29:22 2023 +0800
    
        Draft 3
    
    commit 87522953d598fad5db8521595d17862360a3b7e0
    Author: jiahui0309 <121473213+jiahui0309@users.noreply.github.com>
    Date:   Sat May 27 17:34:18 2023 +0800
    
        Draft 2.2
    
    commit 93f2ef51d88cf5ae159e2c77189ae5e84f1b3b2a
    Author: jiahui0309 <121473213+jiahui0309@users.noreply.github.com>
    Date:   Sat May 27 17:10:24 2023 +0800
    
        Draft 2.1
    
    commit ca9d91799f8af2053db78ac117b0a09fd7786d9b
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Sat May 27 14:55:27 2023 +0800
    
        Draft 2
    
    commit 5a043aa58b2163fd0e2c7a81db65de01506dfa30
    Author: jiahui0309 <121473213+jiahui0309@users.noreply.github.com>
    Date:   Sat May 27 14:29:40 2023 +0800
    
        Draft 1
    
    commit a3d1121610d209dceb44acd42ec7ca8b12d2c840
    Author: tengcharmaine <e0969596@u.nus.edu>
    Date:   Wed May 24 22:13:50 2023 +0800
    
        ms1

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -4,7 +4,7 @@ import { Entypo } from "react-native-vector-icons";
 export default function HomeScreen() {
     return (
         <Tabs>
-            <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+            <Tabs.Screen name="index" options={{ title: "Profile" }} />
             <Tabs.Screen name="submit" options={{ title: "Submit" }} />
             <Tabs.Screen name="binsnearme" options={{ title: "Bins Near Me"}} />
             <Tabs.Screen name="recyclable" options={{ title: "Recyclable?"}} />
