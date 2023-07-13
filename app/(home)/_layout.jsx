fa289ac81025029a77f763101ae83bc3041b1202
jiahui0309 <121473213+jiahui0309@users.noreply.github.com>
2023-07-13 17:32:04 +0800

    animated the tab bar

--- app/(home)/_layout.jsx
+++ app/(home)/_layout.jsx
@@ -1,5 +1,5 @@
-import React, { useRef, useEffect }  from "react";
-import { View, Image, Text, Animated } from "react-native";
+import React, { useRef, useEffect, useState  }  from "react";
+import { View, Image, Text, Animated, TouchableOpacity } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { useNavigation } from "@react-navigation/native";
@@ -14,73 +14,112 @@ const Tab = createBottomTabNavigator();
 
 export default function HomeScreen() {
 
-function CustomTabBarIcon({ iconName, focused, tabName }) {
-  const buttonWidth = tabName == "REWARDS" && focused ? 100 : (focused ? 90 : 50);
-    return (
-    <View
-    style={{
-      flexDirection: "row",
-      alignItems: "center",
-      backgroundColor: focused ? "black" : "white",
-      borderRadius: 15,
-      justifyContent: "center",
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      marginHorizontal: 5,
-      width: buttonWidth,
-      height: 50,
-    }}
-  >
-    <View >
-          <Icon name={iconName} size={focused? 25 : 30} color={focused ? "white" : "black"} />
-        </View>
-        {focused && <Text style={{ color: "white", fontFamily: 'Thonburi-Bold', marginLeft: 5 }}>{tabName}</Text>}
-      </View>
-    );
-  }
+function CustomTabBar({ state, descriptors, navigation }) {
+  const [selectedTab, setSelectedTab] = useState(state.index);
 
-return (
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName = "";
+  const tabScale = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    // Animate tab scale when selectedTab changes
+    Animated.spring(tabScale, {
+      toValue: 1.2,
+      useNativeDriver: true,
+      delay: 0
+    }).start();
+  }, [selectedTab]);
+
+  const handleTabPress = (index) => {
+    if (index !== selectedTab) {
+      // Animate tab scale when a new tab is selected
+      Animated.spring(tabScale, {
+        toValue: 1,
+        useNativeDriver: true,
+        delay: 0
+      }).start(() => {
+        setSelectedTab(index);
+        navigation.navigate(state.routeNames[index]);
+      });
+    }
+  };
+
+  return (
+    <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const isFocused = state.index === index;
+
+        let iconName = "";
             let tabName = "";
             switch (route.name) {
               case "index":
-                iconName = focused ? "account-circle" : "account-circle-outline";
+                iconName = isFocused ? "account-circle" : "account-circle-outline";
                 tabName = "HOME";
                 break;
-            //   case "submit":
-            //     iconName = focused ? "file-document-edit" : "file-document-edit-outline";
-            //     tabName = "Submit";
-            //     break;
               case "binsnearme":
-                iconName = focused ? "map-marker" : "map-marker-outline";
+                iconName = isFocused ? "map-marker" : "map-marker-outline";
                 tabName = "MAP";
                 break;
               case "recyclable":
-                iconName = focused ? "camera" : "camera-outline";
+                iconName = isFocused ? "camera" : "camera-outline";
                 tabName = "SCAN";
                 break;
               case "rewards":
-                iconName = focused ? "gift" : "gift-outline";
+                iconName = isFocused ? "gift" : "gift-outline";
                 tabName = "REWARDS";
                 break;
               case "ranking":
-                iconName = focused ? "podium-gold" : "podium";
+                iconName = isFocused ? "podium-gold" : "podium";
                 tabName = "RANK";
                 break;
               default:
                 break;
             }
-            return <CustomTabBarIcon iconName={iconName} focused={focused} tabName={tabName}/>;
-          },
-            tabBarLabelStyle: { display: "none" },
-            tabBarStyle: { height: "10%", marginLeft: 5, marginRight: 5, justifyContent: "space-between" },
-            tabBarActiveTintColor: "white",
-            tabBarInactiveTintColor: "black",
-            
-        })}
+
+        const iconSize = isFocused ? 30 : 25;
+        const iconColor = isFocused ? "white" : "black";
+
+        const tabScaleValue = isFocused ? tabScale : 1;
+
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => handleTabPress(index)}
+            style={{ alignItems: "center" }}
+          >
+            <Animated.View style={{ transform: [{ scale: tabScaleValue }] }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  backgroundColor: isFocused ? "black" : "white",
+                  borderRadius: 15,
+                  justifyContent: "center",
+                  paddingHorizontal: 10,
+                  paddingVertical: 5,
+                  marginHorizontal: 5,
+                  marginBottom: 20,
+                  width: tabName == "REWARDS" && isFocused ? 110 : (isFocused ? 90 : 50),
+                  height: 50,
+                }}
+              >
+                <Icon name={iconName} size={iconSize} color={iconColor} />
+                {isFocused && (
+                  <Text style={{ color: "white", fontFamily: 'Thonburi-Bold', marginLeft: 5 }}>
+                    {tabName}
+                  </Text>
+                )}
+              </View>
+            </Animated.View>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
+
+return (
+      <Tab.Navigator
+        tabBar={(props) => <CustomTabBar {...props} />}
       >
             <Tab.Screen name="index" component={FriendsStack} options={{ title: "Profile"}} />
             {/* <Tab.Screen name="submit" component={SubmitScreen} options={{ title: "Submit" }} /> */}
